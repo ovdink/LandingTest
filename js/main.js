798c0e3de55ce35020acb1e59229d584121819bf
ovdink <ovdink@example.com>
2019-08-11 00:36:24 +0300

    initial commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(function () {
+    $('.slider-inner').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 8000,
+        nextArrow: '<button type="button" class="slick-btn slick-next"></button>',
+        prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
+        infinite: false,
+    });
+});
